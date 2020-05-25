07a32ea4d396ae0d574bdbb7ec4f5efc6873015c
manojsoni53 <manojsoni2091@gmail.com>
2020-05-25 14:11:33 +0530

    Http Client implemented in ProductService

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit{
     imageWidth:number=25;
     imageMargin:number=2;
     showImage:boolean =false;
+    errorMessage:string;
     
     _listFilter:string;
     get listFilter() : string{
@@ -40,8 +41,14 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void{
-      this.products=this.productService.getProduct();
-      this.filteredProducts = this.products;
+      this.productService.getProduct().subscribe({
+        next: products => {
+          this.products = products;
+          this.filteredProducts = this.products;
+        },
+        error: err => this.errorMessage = err
+      });
+      
     }
 
     onRatingclicked(message:string): void{
