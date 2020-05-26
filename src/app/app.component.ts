8af02db5f0e6743c05206649d346a1da66b0c1aa
manojsoni53 <manojsoni2091@gmail.com>
2020-05-27 02:05:56 +0530

    configure the routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-roots',
   template: `
-    <div><h1>{{pageTitle}}</h1>
-    <pm-product></pm-product>
-    </div>
+  <nav class='navbar navbar-expand navbar-light bg-light'>
+  <a class='navbar-brand'>{{pageTitle}}</a>
+    <ul class='nav nav-pills'>
+      <li><a class='nav-link' [routerLink]="['/welcome']">Home</a></li>
+      <li><a class='nav-link' [routerLink]="['/products']">ProductList</a></li>
+    </ul>
+  </nav>
+  <div class='container'>
+    <router-outlet></router-outlet>
+  </div>
+
   `
 })
 export class AppComponent {
