575aadd51f2becdd3cc419e9dbdcaa9eef9e1c54
manojsoni53 <manojsoni2091@gmail.com>
2020-05-29 01:06:07 +0530

    implemented product-detail view

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -10,9 +11,22 @@ export class ProductDetailComponent implements OnInit {
   pageTitle:string = 'Product Detail';
   product:IProduct;
 
-  constructor() { }
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    let id = +this.route.snapshot.paramMap.get('id');   //the + is javascript shortcut to convert the parameter string to a numeric Id
+    this.pageTitle +=` : ${id}`;
+    this.product = {
+    "productId": id,
+    "productName": "Leaf Rake",
+    "productCode": "GDN-0011",
+    "releaseDate": "March 19, 2019",
+    "description": "Leaf rake with 48-inch wooden handle.",
+    "price": 19.95,
+    "starRating": 3.2,
+    "imageUrl": "assets/images/leaf_rake.png"
+
+    }
   }
 
 }
